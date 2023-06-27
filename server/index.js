86d4164b3af61f25b894b79d39ae9fb9db23eb85
Pranavbajpai38 <108859124+Pranavbajpai38@users.noreply.github.com>
2023-06-27 16:05:49 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import multer from "multer"
+import helmet from "helmet"
+import morgan from "morgan"
+import path from "path"
+import { fileURLToPath } from "url"
+import { error } from "console"
+import{register} from "./controllers/auth.js"
+import{createPost} from "./controllers/posts.js"
+import authroutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import { verifyToken } from "./middleware/auth.js"
+import User from './models/Users.js'
+import Post from "./models/posts.js"
+import {users,posts} from './data/index.js'
+const __filename=fileURLToPath(import.meta.url)
+const __diname = path.dirname(__filename)
+dotenv.config();
+const app =express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+app.use("/assets",express.static(path.join(__diname,'public/assets')))
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb)
+    {
+        cb(null,"public/assets");
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    }
+})
+
+const upload = multer({storage})
+
+
+
+
+
+app.post("/auth/register",upload.single("picture"),register)
+
+app.post("/posts",verifyToken,upload.single("picture"),createPost)
+/*routes*/
+
+app.use("/auth",authroutes)
+app.use("/users",userRoutes)
+app.use("/posts",postRoutes)
+
+
+const PORT =process.env.PORT|| 6001;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+})
+.then(()=>{
+    app.listen(PORT,()=>console.log(`Server Port: ${PORT}`))
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+})
+.catch((error)=>console.log(`${error} did not connect`))
